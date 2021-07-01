1a443aaf73c31e353b44e49a406179c24670f540
Rayan Castro <rayanrcastro@gmail.com>
2021-07-01 17:59:43 -0300

    chore: add suport id with uuid on migrations

--- config/config.exs
+++ config/config.exs
@@ -10,6 +10,10 @@ use Mix.Config
 config :twitter,
   ecto_repos: [Twitter.Repo]
 
+  config :twitter, Twitter.Repo,
+    migration_primary_key: [type: :binary_id],
+    migration_foreign_key: [type: :binary_id]
+
 # Configures the endpoint
 config :twitter, TwitterWeb.Endpoint,
   url: [host: "localhost"],
