31ee2d1325f1ef7e73905c003e32b61e1c897872
Rayan Castro <rayanrcastro@gmail.com>
2021-07-04 17:16:12 -0300

    chore: add use case for unfollow user

---
+++ lib/twitter/usecases/unfollow_user.ex
@@ -0,0 +1,20 @@
+defmodule Twitter.UseCases.UnfollowUser do
+  import Ecto.Query
+
+  alias Twitter.Schemas.UserFollow
+
+  def call(params) do
+    params
+    |> build_query()
+    |> Twitter.Repo.delete_all()
+    |> handle_delete()
+  end
+
+  defp build_query(%{"from_id" => from_id, "to_id" => to_id}) do
+    from(UserFollow, where: [from_id: ^from_id, to_id: ^to_id])
+  end
+
+  defp handle_delete({0, nil}), do: {:error, %{detail: ["user follow not exists"]}}
+
+  defp handle_delete({1, nil}), do: :ok
+end
