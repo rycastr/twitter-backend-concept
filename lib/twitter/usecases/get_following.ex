bc82499ef2bc5c06831f65cb43aa4b7c8dd2a010
Rayan Castro <rayanrcastro@gmail.com>
2021-07-06 17:07:16 -0300

    refactor: replace the function name get_from for get_to

--- lib/twitter/usecases/get_following.ex
+++ lib/twitter/usecases/get_following.ex
@@ -8,12 +8,12 @@ defmodule Twitter.UseCases.GetFollowing do
     from(uf in UserFollow, where: uf.from_id == ^from_id)
     |> Twitter.Repo.all()
     |> Repo.preload(to: from(u in User, select: map(u, [:id, :name, :username])))
-    |> Enum.map(&get_from/1)
+    |> Enum.map(&get_to/1)
     |> handle_followers()
     # |> Twitter.Repo.preload([:from])
   end
 
-  defp get_from(%UserFollow{to: to}), do: to
+  defp get_to(%UserFollow{to: to}), do: to
 
   defp handle_followers(followers) do
     {:ok, %{following_count: length(followers), following: followers}}
